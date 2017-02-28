d452def3a9e22883b8ca4d10fe2d285513f3b86a
MaximeRemy <maximeremy@yahoo.fr>
2017-02-28 16:07:33 +0100

    Add the matlab script and the data to find the inductance of the DC motor

---
+++ GearTrain/GearTrainTest/LaPlot.m
@@ -0,0 +1,29 @@
+clear all;
+%% READ MEASURED DATA
+[FileA,PathA]=uigetfile('*','Select measured frequency response file');
+FullFileA = fullfile(PathA,FileA);
+[T V]=textread(FullFileA,'%f%f','headerlines',2);
+
+
+%% Plotting the current in respect to time
+R= 0.01; %Value of the shunt resistor
+I=V/R; 
+plot (T, I, '--b');
+xlabel('Time [s]'); ylabel('Current [A]');
+axis tight;
+grid;
+
+
+%% Finfing tau
+AfterStep=20.470600000000000; %value deduced by the mean of the end values
+BeforeStep=-0.357100000000000; %value deduced by the mean of the values before the step
+
+Step=AfterStep-BeforeStep;
+
+[idx idx]=min(abs(I-Step*0.632));
+
+tau=T(idx)-T(1);
+
+%% Findind La
+
+La=tau*R*10^6
