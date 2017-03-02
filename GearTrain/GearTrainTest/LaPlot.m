0e726282710ab2b0ae0fc063854f62d48b60425e
MaximeRemy <maximeremy@yahoo.fr>
2017-03-02 10:00:46 +0100

    Add the test journal for the Internal inductance of the DC motor test

--- GearTrain/GearTrainTest/LaPlot.m
+++ GearTrain/GearTrainTest/LaPlot.m
@@ -7,23 +7,24 @@ FullFileA = fullfile(PathA,FileA);
 
 %% Plotting the current in respect to time
 R= 0.01; %Value of the shunt resistor
+Ra=0.43; %Value of the internal restor of the DC motor
 I=V/R; 
-plot (T, I, '--b');
+plot (T, I);
 xlabel('Time [s]'); ylabel('Current [A]');
 axis tight;
 grid;
 
 
-%% Finfing tau
+%% Finding tau
 AfterStep=20.470600000000000; %value deduced by the mean of the end values used brushing function of matlab to get the data
 BeforeStep=-0.357100000000000; %value deduced by the mean of the values before the step
 
 Step=AfterStep-BeforeStep;
 
-[idx idx]=min(abs(I-Step*0.632));
+[idx idx]=min(abs(I-AfterStep*0.632));
 
 tau=T(idx)-T(1);
 
 %% Findind La
 
-La=tau*R*10^6
+La=tau*Ra*10^6
