487230082963de450c1d3c791c1c4006359f61f1
MaximeRemy <maximeremy@yahoo.fr>
2017-03-23 15:37:19 +0100

    Some Script for the moment of inertia test

---
+++ GearTrain/GearTrainTest/simJm.m
@@ -0,0 +1,11 @@
+function  y=simJm(u,t,par)
+% y=simktau(u,t,par) simulates K/(1+s*tau) using lsim
+% par=[K tau]
+% Example: par=[2 3] inputt='inputsq' f1=.02 maintests
+%
+% 31/10-02,MK
+
+J=par(1);
+nc=Kt; dc=[Jm*Lm Jm*Rm+Bm*Lm Bm*Rm+Kt*Ke];
+t=[0 t(1:length(t)-1)];
+y=lsim(nc,dc,u,t);
