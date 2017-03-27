65cc8a300ecd418f0a38e449238530882fa1342a
GeoffSion <gsion16@student.aau.dk>
2017-03-27 16:18:24 +0200

    (ノಠ益ಠ)ノ彡┻━┻

--- GearTrain/GearTrainTest/LoadJmData.m
+++ GearTrain/GearTrainTest/LoadJmData.m
@@ -1,5 +1,8 @@
 clear all;
 %% READ MEASURED DATA
-[FileA,PathA]=uigetfile('*','Select measured frequency response file');
+[FileA,PathA]=uigetfile('*','Select measured DC motor output');
 FullFileA = fullfile(PathA,FileA);
-[y t]=textread(FullFileA,'%f,%f','headerlines',2);
+[t y]=textread(FullFileA,'%f,%f','headerlines',6);
+[FileA,PathA]=uigetfile('*','Select measured input');
+FullFileA = fullfile(PathA,FileA);
+[T u]=textread(FullFileA,'%f,%f','headerlines',6);
