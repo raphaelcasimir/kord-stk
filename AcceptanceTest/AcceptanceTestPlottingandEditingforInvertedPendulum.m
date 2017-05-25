4a386ad7db37bd69f0989e54cc15ef3e58626787
MaximeRemy <maximeremy@yahoo.fr>
2017-05-25 08:28:15 +0200

    Began the acceptance test section for the inv pend

--- AcceptanceTest/AcceptanceTestPlottingandEditingforInvertedPendulum.m
+++ AcceptanceTest/AcceptanceTestPlottingandEditingforInvertedPendulum.m
@@ -20,18 +20,11 @@ mappingConstArm=(-117.0);
 mappingSlopeStick=(66.66/204.8);
 mappingConstStick=(-163.9);
 
-UpLimitStick=zeros(length(ArmAngle));
-DownLimitStick=zeros(length(ArmAngle));
+UpLimitStick=0.087222222;
+DownLimitStick=-0.087222222;
 
-UpLimitArm=zeros(length(ArmAngle));
-DownLimitArm=zeros(length(ArmAngle));
-
-for i=1:length(UpLimitArm)
-	UpLimitStick(i)=0.087222222;
-	DownLimitStick(i)=-0.087222222;
-	UpLimitArm(i)=0.785;
-	DownLimitArm(i)=-0.785;
-end
+UpLimitArm=0.785;
+DownLimitArm=-0.785;
 
 
 ArmAngle=deg2rad(ArmAngle.*mappingSlopeArm+mappingConstArm);
@@ -39,11 +32,23 @@ AngleStick=deg2rad(AngleStick.*mappingSlopeStick+mappingConstStick)+ArmAngle;
 
 %plot
 figure ('Name','AngleArm')
-plot(t,ArmAngle,t,UpLimitArm ,'--', 'MarkerSize', 18, 'LineWidth', 3);
+h1=plot(t,ArmAngle,'MarkerSize', 18, 'LineWidth', 3);
+hold on;
+h2=refline(0,UpLimitArm);
+set(h2,'LineStyle','--','MarkerSize',18,'LineWidth',3,'Color','r');
+h3=refline(0,DownLimitArm);
+set(h3,'LineStyle','--','MarkerSize',18,'LineWidth',3,'Color','r');
+h4=refline(0,max(ArmAngle));
+set(h4,'LineStyle','--','MarkerSize',18,'LineWidth',2,'Color','b');
+h5=refline(0,min(ArmAngle));
+set(h5,'LineStyle','--','MarkerSize',18,'LineWidth',2,'Color','b');
+xlabel('Time seconds', 'FontSize', 14);
 xlabel('Time seconds', 'FontSize', 14);
 ylabel('Arm Angle radians', 'FontSize', 14);
 ylim([-0.85 0.85]);
 title('Angle of the Arm over Time', 'FontSize', 16)
+legend([h1 h2 h4],{'Angle of the Arm','Requirement', 'Maximum & Minimum Angle of the Arm'},'FontSize',12)
+hold off;
 %p=getoptions(h);
 %p.ylabel.FontSize = 14;
 %p.xlabel.FontSize = 14;
@@ -52,8 +57,19 @@ title('Angle of the Arm over Time', 'FontSize', 16)
 %p.ticklabel.FontSize = 12;
 
 figure ('Name','AngleStick')
-plot(t,AngleStick, 'MarkerSize', 18, 'LineWidth', 3);
+h1=plot(t,AngleStick, 'MarkerSize', 18, 'LineWidth', 3);
+hold on;
+h2=refline(0,UpLimitStick);
+set(h2,'LineStyle','--','MarkerSize',18,'LineWidth',3,'Color','r');
+h3=refline(0,DownLimitStick);
+set(h3,'LineStyle','--','MarkerSize',18,'LineWidth',3,'Color','r');
+h4=refline(0,max(AngleStick));
+set(h4,'LineStyle','--','MarkerSize',18,'LineWidth',2,'Color','b');
+h5=refline(0,min(AngleStick));
+set(h5,'LineStyle','--','MarkerSize',18,'LineWidth',2,'Color','b');
 xlabel('Time seconds', 'FontSize', 14);
 ylabel('Stick Angle radians', 'FontSize', 14);
 ylim([-0.1 0.1]);
 title('Angle of the Stick over Time', 'FontSize', 16)
+legend([h1 h2 h4],{'Angle of the Stick','Requirement', 'Maximum & Minimum Angle of the Stick'},'FontSize',12)
+hold off;
